353ee6adb6b401c8de2be3b6b0c75b4daf59bf9a
smarulanda97 <santiagomm1997@gmail.com>
2021-05-17 12:15:40 -0500

    Adding application types

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,11 @@
+export enum ButtonColors {
+  primary = 'primary',
+  secondary = 'secondary',
+}
+
 export type LinkComponentProps = {
   text: string;
-  color: string;
+  color: ButtonColors;
   href?: string;
   target?: string;
   className?: string;
