ccda5a05c2cf93607616a1723309ed4e2d810539
smarulanda97 <santiagomm1997@gmail.com>
2021-07-08 02:33:10 -0500

    First SEO improve Example

--- components/App/App.tsx
+++ components/App/App.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
+import { NextSeo } from 'next-seo';
 import { Layout } from '@components/index';
 import { AppProps } from '@types-app/index';
+import { withRouter, NextRouter } from 'next/router';
 
-class App extends React.Component<AppProps> {
+interface WithRouterProps extends AppProps {
+  router: NextRouter;
+}
+
+class App extends React.Component<WithRouterProps> {
   render() {
-    const { layout, children } = this.props;
+    const { layout, children, router, url } = this.props;
+
+    const tags = {
+      title: 'Spa 5 Sentidos',
+      description: 'Spa 5 Sentidos es un Spa',
+      defaultTitle: 'Spa 5 Sentidos - Servicio a domicilio de masajes',
+      openGraph: {
+        type: 'website',
+        title: 'Spa 5 Sentidos',
+        url: url.origin + router.pathname,
+        defaultTitle: 'Spa 5 Sentidos - Servicio a domicilio de masajes',
+      },
+    };
 
     return (
-      <div className={'app'} id={'app'}>
-        {layout ? <Layout>{children}</Layout> : children}
-      </div>
+      <>
+        <NextSeo {...tags} />
+        <div className={'app'} id={'app'}>
+          {layout ? <Layout>{children}</Layout> : children}
+        </div>
+      </>
     );
   }
 }
 
-export default React.memo(App);
+export default React.memo(withRouter(App));
