c64f249bc85dc9bd274b5a4f21a0a5419489c7de
Santiago Marulanda <santiagomm1997@gmail.com>
2021-11-08 02:41:18 -0500

    Update path aliases

--- jest.config.js
+++ jest.config.js
@@ -17,18 +17,18 @@ module.exports = {
     /**
      * Path aliases
      */
-    '^@hoc/(.*)': '<rootDir>/hoc/$1',
-    '^@lib/(.*)': '<rootDir>/lib/$1',
-    '^@utils/(.*)': '<rootDir>/utils/$1',
-    '^@hooks/(.*)': '<rootDir>/hooks/$1',
-    '^@pages/(.*)': '<rootDir>/pages/$1',
-    '^@styles/(.*)': '<rootDir>/styles/$1',
-    '^@context/(.*)': '<rootDir>/context/$1',
-    '^@components/(.*)': '<rootDir>/components/$1',
-    '^@queries/(.*)': '<rootDir>/queries/$1',
-    '^@mocks/(.*)': '<rootDir>/mocks/$1',
-    '^@test/(.*)': '<rootDir>/test/$1',
-    '^@types-app/(.*)': '<rootDir>/types/$1',
+    '^@hoc/(.*)': '<rootDir>/src/hoc/$1',
+    '^@lib/(.*)': '<rootDir>/src/lib/$1',
+    '^@utils/(.*)': '<rootDir>/src/utils/$1',
+    '^@hooks/(.*)': '<rootDir>/src/hooks/$1',
+    '^@pages/(.*)': '<rootDir>/src/pages/$1',
+    '^@styles/(.*)': '<rootDir>/src/styles/$1',
+    '^@context/(.*)': '<rootDir>/src/context/$1',
+    '^@components/(.*)': '<rootDir>/src/components/$1',
+    '^@queries/(.*)': '<rootDir>/src/queries/$1',
+    '^@mocks/(.*)': '<rootDir>/src/mocks/$1',
+    '^@test/(.*)': '<rootDir>/src/test/$1',
+    '^@types-app/(.*)': '<rootDir>/src/types/$1',
     /**
      * Mocks
      */
