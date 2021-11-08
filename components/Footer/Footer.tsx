27ae751b58d669c56690089a639fa803fd6a22ac
Santiago Marulanda <santiagomm1997@gmail.com>
2021-11-08 01:53:38 -0500

    Update Footer component and its styles

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { asset } from '@utils/imageUtils';
 import { useTranslation } from 'next-i18next';
+import { Container, Row, Col } from 'react-bootstrap';
+
+import { asset } from '@utils/index';
 import { Navigation } from '@components/index';
 import { StrapiImage, Menu } from '@types-app/index';
-import { Container, Row, Col } from 'react-bootstrap';
 
 import styles from './Footer.module.scss';
 
 type Props = {
   socialMenu: Menu;
+  contactMenu: Menu;
+  openingMenu: Menu;
+  description: string;
   logoFooter: StrapiImage;
-  children?: JSX.Element | JSX.Element[];
 };
 
 const Footer: React.FC<Props> = (props) => {
   const { t } = useTranslation('common');
-  const { children, logoFooter, socialMenu } = props;
+  const { logoFooter, socialMenu, openingMenu, contactMenu, description } =
+    props;
 
   return (
     <footer className={styles.footer}>
       <Container>
         <Row>
-          <Col>
+          <Col xs={12} md={6} lg={3}>
             {/** Render footer logo */}
             {logoFooter && (
               <Link href={'/'}>
@@ -37,24 +41,44 @@ const Footer: React.FC<Props> = (props) => {
                 </a>
               </Link>
             )}
+            <p>{description}</p>
           </Col>
-        </Row>
-        <Row>
-          <Col>
-            {children}
-            {/** Render social menu <Navigation /> */}
+          <Col xs={12} md={6} lg={3}>
+            <h2>{t('important_links')}</h2>
+          </Col>
+          <Col xs={12} md={6} lg={3}>
+            <h2>{t('opening_hours')}</h2>
+            {openingMenu && (
+              <Navigation
+                menu={openingMenu}
+                testId={'opening-menu-footer'}
+                className={'menu-opening my-4'}
+              />
+            )}
+          </Col>
+          <Col xs={12} md={6} lg={3}>
+            <h2>{t('contact_us')}</h2>
+            {contactMenu && (
+              <Navigation
+                menu={contactMenu}
+                testId={'contact-menu-footer'}
+                className={'menu-contact my-4'}
+              />
+            )}
             {socialMenu && (
               <Navigation
                 menu={socialMenu}
                 testId={'social-menu-footer'}
-                className={'social-menu my-4'}
+                className={'menu-social my-4'}
               />
             )}
-            {/** Render copy right */}
+          </Col>
+        </Row>
+        <Row>
+          <Col>
             <p className={'text-center'}>
               <span>© Spa 5 Sentidos. &nbsp;</span>
               <span>{t('all_rights_reserved')}. &nbsp;</span>
-              <span>{t('powered_by', { author: 'smarulanda97' })}.</span>
             </p>
           </Col>
         </Row>
